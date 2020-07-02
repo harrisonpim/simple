981790cf3987ac11dd2f0bb971b810249a1cfd46
harrisonpim <harrison.stroma@gmail.com>
2020-07-02 19:43:39 +0100

    add support for punctuation and emoji

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React, { Component } from "react";
 import Simple from "./Simple";
-import { vocabulary } from "./data.json";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { vocabulary: vocabulary };
-  }
-
   render() {
     return (
       <div className="pv4 ph4 ph5-l black-80 sans-serif vh-100">
@@ -15,13 +9,11 @@ class App extends Component {
           placeholder="write some big, simple words for the top"
           className="f2 f1-l lh-title b"
           wordLimit={10}
-          vocabulary={this.state.vocabulary}
         />
         <Simple
           placeholder="write some more simple words down here"
           className="f4 lh-copy measure-wide"
           wordLimit={1000}
-          vocabulary={this.state.vocabulary}
         />
       </div>
     );
--- src/Simple.js
+++ src/Simple.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import ContentEditable from "react-contenteditable";
 import sanitizeHtml from "sanitize-html";
+import { emoji, punctuation, vocabulary } from "./data.json";
 import "./simple.css";
 
 class Simple extends Component {
   constructor(props) {
     super(props);
-    this.state = { html: "" };
+    this.state = {
+      html: "",
+      punctuation: punctuation,
+      vocabulary: vocabulary.concat(emoji),
+    };
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  tokenise(inputHTML) {
+    // remove html tags and tokenise text
+    let cleanText = sanitizeHtml(inputHTML, {
+      allowedTags: [],
+      allowedAttributes: {},
+    }).toLowerCase();
+    return cleanText.split(" ");
   }
 
   labelTokens(tokens) {
@@ -15,7 +29,12 @@ class Simple extends Component {
     let labelledTokens = [];
     for (var i = 0; i < tokens.length; i++) {
       let token = tokens[i];
-      if (this.props.vocabulary.includes(token)) {
+      if (this.state.vocabulary.includes(token)) {
+        labelledTokens.push(token);
+      } else if (
+        this.state.vocabulary.includes(token.slice(0, -1)) &
+        this.state.punctuation.includes(token.slice(-1))
+      ) {
         labelledTokens.push(token);
       } else {
         labelledTokens.push(`<span>${token}</span>`);
@@ -25,15 +44,13 @@ class Simple extends Component {
   }
 
   handleChange(event) {
-    // remove html tags and tokenise text
-    let cleanText = sanitizeHtml(event.target.value, {
-      allowedTags: [],
-      allowedAttributes: {},
-    });
-    let tokens = cleanText.split(" ");
+    let tokens = this.tokenise(event.target.value);
     let labelledHTML = this.labelTokens(tokens).join(" ");
-
     this.setState({ html: labelledHTML });
+  }
+
+  handleBlur(event) {
+    console.log(event.type);
   }
 
   render() {
@@ -43,7 +60,7 @@ class Simple extends Component {
         placeholder={this.props.placeholder}
         html={this.state.html}
         onChange={this.handleChange}
-        onBlur={this.handleChange}
+        onBlur={this.handleBlur}
       />
     );
   }
